fec3eacc7250ded63b136a3b0f5f0bbc74cc1445
gmougeot <guillaume.mougeot@laposte.net>
2018-11-21 20:18:43 +0800

    Load data into numpy arrays

--- dogfacenet/model.py
+++ dogfacenet/model.py
@@ -9,6 +9,7 @@ Written by Guillaume Mougeot
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from skimage.transform import resize
 
 import tensorflow as tf
 
@@ -31,15 +32,39 @@ for file in os.listdir(PATH_BG):
     if ".jpg" in file:
         filenames_bg += [file]
 
-filename_dog1 = []
+filenames_dog1 = []
 for file in os.listdir(PATH_DOG1):
     if ".jpg" in file:
-        filenames_bg += [file]
+        filenames_dog1 += [file]
 
+
+IM_H = 224
+IM_W = 224
+IM_C = 3
+
+# image = tf.image.decode_jpeg(PATH_BG + filenames_bg[0], channels=3)
+# resized_image = tf.image.resize_images(image, [IM_H, IM_W])
+# print(resized_image)
 
 # Load images into Numpy arrays
 
-images_bg = KI.img_to_array(KI.load_img(PATH_BG + filenames_bg[0]))
+# Load background images
+image = KI.img_to_array(KI.load_img(PATH_BG + filenames_bg[0]))/255
+image = resize(image, (IM_H, IM_W), mode='reflect')
+images_bg = np.array([image])
+for i in range(1, len(filenames_bg)):
+    image = KI.img_to_array(KI.load_img(PATH_BG + filenames_bg[i]))/255
+    image = resize(image, (IM_H, IM_W), mode='reflect')
+    np.append(images_bg, [image], axis=0)
+
+# Load dog 1 images
+image = KI.img_to_array(KI.load_img(PATH_DOG1 + filenames_dog1[0]))/255
+image = resize(image, (IM_H, IM_W), mode='reflect')
+images_dog1 = np.array([image])
+for i in range(1, len(filenames_dog1)):
+    image = KI.img_to_array(KI.load_img(PATH_DOG1 + filenames_dog1[i]))/255
+    image = resize(image, (IM_H, IM_W), mode='reflect')
+    np.append(images_dog1, [image], axis=0)
 
 
 ############################################################
@@ -58,8 +83,18 @@ def NASNet_embedding(
     x = KL.Dense(1056, activation='relu')(x)
     x = KL.Dropout(0.5)(x)
     x = KL.Dense(128)(x)
-    #model = tf.keras.Model(inputs=base_model.input, outputs=x)
+
     return x
+
+# With earger execution:
+# tf.enable_eager_execution()
+
+# model = tf.keras.Sequential([
+#   KA.NASNetMobile(input_shape=(224,224,3), include_top=False),
+#   KL.GlobalAveragePooling2D(),
+#   KL.Dense(1056, activation='relu'),
+#   KL.Dense(128)
+# ])
 
 
 ############################################################
