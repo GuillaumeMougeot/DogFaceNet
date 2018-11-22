bd43ccdc73c9103e1c68ea3636947a6d3d881f8b
gmougeot <guillaume.mougeot@laposte.net>
2018-11-22 16:11:47 +0800

    Change loading data method

--- dogfacenet/model.py
+++ dogfacenet/model.py
@@ -6,6 +6,10 @@ Licensed under the MIT License (see LICENSE for details)
 Written by Guillaume Mougeot
 """
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import os
 import numpy as np
 import matplotlib.pyplot as plt
@@ -45,68 +49,83 @@ IM_H = 224
 IM_W = 224
 IM_C = 3
 
-# Method 1: Using tensorflow methods
-"""
+#### Method 1: Using tensorflow methods
+
 def _parse_function(filename, label):
     image_string = tf.read_file(filename)
     image_decoded = tf.image.decode_jpeg(image_string, channels=3)
     image_resized = tf.image.resize_images(image_decoded, [IM_H, IM_W])
     return image_resized, label
 
-filenames = tf.constant(filenames_dog1 + filenames_bg)
-labels = tf.constant(np.append(np.ones(len(filenames_dog1)), np.arange(2,2+len(filenames_bg))))
+filenames = np.append(filenames_dog1, filenames_bg, axis=0)
+labels = np.append(np.ones(len(filenames_dog1)), np.arange(2,2+len(filenames_bg)))
 
-dataset = tf.data.Dataset.from_tensor_slices((filenames, labels))
-dataset = dataset.map(_parse_function)
-"""
-
-# Method 2: using standard techniques
-# Load images into Numpy arrays
-
-def load_imgs(path, filenames, new_shape=(224,224), mode='constant'):
-    """Load an image defined by filenames from a certain path and reshape it 
-    with the precised mode"""
-
-    image = KI.img_to_array(KI.load_img(path + filenames[0]))/255
-    image = resize(image, new_shape, mode=mode)
-    images = np.array([image])
-    for i in tqdm(range(1, len(filenames))):
-        image = KI.img_to_array(KI.load_img(path + filenames[i]))/255
-        image = resize(image, new_shape, mode=mode)
-        images = np.append(images, [image], axis=0)
-    return images
-
-# Load background images
-images_bg = load_imgs(PATH_BG, filenames_bg, (IM_H, IM_W))
-
-# Load dog 1 images
-images_dog1 = load_imgs(PATH_DOG1, filenames_dog1, (IM_H, IM_W))
-
-# Define the identies of the dogs: dog1->1, bg->[2,len(bg)]
-labels_bg = np.arange(2, len(images_bg)+2)
-labels_dog1 = np.ones(len(images_dog1))
-
-# Queue images and labels
-images = np.append(images_dog1, images_bg)
-labels = np.append(labels_dog1, labels_bg)
-
-# Method 1: Load data into Tensorflow tensor
-# dataset = tf.data.Dataset.from_tensor_slices((images, labels))
-
-# Method 2: Use placeholder
-assert images.shape[0] == labels.shape[0]
-
-# Graph elements:
-images_placeholder = tf.placeholder(images.dtype, images.shape)
+# Filenames and labels place holder
+filenames_placeholder = tf.placeholder(filenames.dtype, filenames.shape)
 labels_placeholder = tf.placeholder(labels.dtype, labels.shape)
 
-# Graph elements:
-dataset = tf.data.Dataset.from_tensor_slices((images_placeholder, labels_placeholder))
+# Defining dataset
+dataset = tf.data.Dataset.from_tensor_slices((filenames_placeholder, labels_placeholder))
+dataset = dataset.map(_parse_function)
 
-# Graph elements:
-iterator = dataset.make_initializable_iterator()
+
+# Plot the tensorboard graph
+# writer = tf.summary.FileWriter('.')
+# writer.add_graph(tf.get_default_graph())
+
+#### End method 1
+
+
+#### Method 2: using standard techniques
+
+# Load images into Numpy arrays
+
+# def load_imgs(path, filenames, new_shape=(224,224), mode='constant'):
+#     """Load an image defined by filenames from a certain path and reshape it 
+#     with the precised mode"""
+
+#     image = KI.img_to_array(KI.load_img(path + filenames[0]))/255
+#     image = resize(image, new_shape, mode=mode)
+#     images = np.array([image])
+#     for i in tqdm(range(1, len(filenames))):
+#         image = KI.img_to_array(KI.load_img(path + filenames[i]))/255
+#         image = resize(image, new_shape, mode=mode)
+#         images = np.append(images, [image], axis=0)
+#     return images
+
+# # Load background images
+# images_bg = load_imgs(PATH_BG, filenames_bg, (IM_H, IM_W))
+
+# # Load dog 1 images
+# images_dog1 = load_imgs(PATH_DOG1, filenames_dog1, (IM_H, IM_W))
+
+# # Define the identies of the dogs: dog1->1, bg->[2,len(bg)]
+# labels_bg = np.arange(2, len(images_bg)+2)
+# labels_dog1 = np.ones(len(images_dog1))
+
+# # Queue images and labels
+# images = np.append(images_dog1, images_bg)
+# labels = np.append(labels_dog1, labels_bg)
+
+# # Method 1: Load data into Tensorflow tensor
+# # dataset = tf.data.Dataset.from_tensor_slices((images, labels))
+
+# # Method 2: Use placeholder
+# assert images.shape[0] == labels.shape[0]
+
+# # Graph elements:
+# images_placeholder = tf.placeholder(images.dtype, images.shape)
+# labels_placeholder = tf.placeholder(labels.dtype, labels.shape)
+
+# # Graph elements:
+# dataset = tf.data.Dataset.from_tensor_slices((images_placeholder, labels_placeholder))
+
+# # Graph elements:
+# iterator = dataset.make_initializable_iterator()
 
 # sess.run(iterator.initializer, feed_dict={images_placeholder: images, labels_placeholder: labels})
+
+#### End Method 2
 
 
 ############################################################
