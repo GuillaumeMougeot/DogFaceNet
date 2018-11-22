c8beb25231d22e4f4f845d71bf10354ebcf4ce53
gmougeot <guillaume.mougeot@laposte.net>
2018-11-22 21:03:53 +0800

    Solve an indentation problem

--- dogfacenet/model.py
+++ dogfacenet/model.py
@@ -37,13 +37,13 @@ PATH_DOG1 = "..\\data\\dog1\\"
 # Retrieve filenames
 filenames_bg = []
 for file in os.listdir(PATH_BG):
-    if ".jpg" in file:
-        filenames_bg += [file]
+        if ".jpg" in file:
+                filenames_bg += [file]
 
 filenames_dog1 = []
 for file in os.listdir(PATH_DOG1):
-    if ".jpg" in file:
-        filenames_dog1 += [file]
+        if ".jpg" in file:
+                filenames_dog1 += [file]
 
 
 IM_H = 224
@@ -52,10 +52,10 @@ IM_C = 3
 
 
 def _parse_function(filename, label):
-    image_string = tf.read_file(filename)
-    image_decoded = tf.image.decode_jpeg(image_string, channels=3)
-    image_resized = tf.image.resize_images(image_decoded, [IM_H, IM_W])
-    return image_resized, label
+        image_string = tf.read_file(filename)
+        image_decoded = tf.image.decode_jpeg(image_string, channels=3)
+        image_resized = tf.image.resize_images(image_decoded, [IM_H, IM_W])
+        return image_resized, label
 
 filenames = np.append([PATH_DOG1 + filenames_dog1[i] for i in range(len(filenames_dog1))],
                 [PATH_BG + filenames_bg[i] for i in range(len(filenames_bg))],
@@ -79,20 +79,21 @@ dataset = dataset.map(_parse_function)
 # Build the model using Keras pretrained model NASNetMobile,
 # a light and efficient network
 def NASNet_embedding(input_tensor,
-  input_shape=(224,224,3),
-  include_top=False,
-  training=True
-):
-    base_model = KA.NASNetMobile(input_tensor=input_tensor,
+        input_shape=(224,224,3),
+        include_top=False,
+        training=True
+        ):
+
+        base_model = KA.NASNetMobile(input_tensor=input_tensor,
                                 input_shape=input_shape,
                                 include_top=False)
-    x = KL.GlobalAveragePooling2D()(base_model.output)
-    x = KL.Dense(1056, activation='relu')(x)
-    if training:
-        x = KL.Dropout(0.5)(x)
-    x = KL.Dense(128)(x)
+        x = KL.GlobalAveragePooling2D()(base_model.output)
+        x = KL.Dense(1056, activation='relu')(x)
+        if training:
+                x = KL.Dropout(0.5)(x)
+        x = KL.Dense(128)(x)
 
-    return x
+        return x
 
 dataset = dataset.batch(32)
 it = dataset.make_initializable_iterator()
@@ -113,21 +114,21 @@ print(sess.run(y_pred))
 
 
 def triplet_loss(anchor, positive, negative, alpha):
-    """Calculate the triplet loss according to the FaceNet paper
-    
-    Args:
-      anchor: the embeddings for the anchor images.
-      positive: the embeddings for the positive images.
-      negative: the embeddings for the negative images.
-  
-    Returns:
-      the triplet loss according to the FaceNet paper as a float tensor.
-    """
-    with tf.variable_scope('triplet_loss'):
-        pos_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, positive)), 1)
-        neg_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, negative)), 1)
-        
-        basic_loss = tf.add(tf.subtract(pos_dist,neg_dist), alpha)
-        loss = tf.reduce_mean(tf.maximum(basic_loss, 0.0), 0)
-      
-    return loss
+        """Calculate the triplet loss according to the FaceNet paper
+
+        Args:
+        anchor: the embeddings for the anchor images.
+        positive: the embeddings for the positive images.
+        negative: the embeddings for the negative images.
+
+        Returns:
+        the triplet loss according to the FaceNet paper as a float tensor.
+        """
+        with tf.variable_scope('triplet_loss'):
+                pos_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, positive)), 1)
+                neg_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, negative)), 1)
+
+                basic_loss = tf.add(tf.subtract(pos_dist,neg_dist), alpha)
+                loss = tf.reduce_mean(tf.maximum(basic_loss, 0.0), 0)
+
+        return loss
