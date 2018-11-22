f3ee36a1e71c1919c235709f270e5e716e9f0ca1
gmougeot <guillaume.mougeot@laposte.net>
2018-11-22 21:00:24 +0800

    A first functional and clean method

--- dogfacenet/model.py
+++ dogfacenet/model.py
@@ -33,6 +33,7 @@ PATH_DOG1 = "..\\data\\dog1\\"
 #  Data analysis
 ############################################################
 
+
 # Retrieve filenames
 filenames_bg = []
 for file in os.listdir(PATH_BG):
@@ -49,7 +50,6 @@ IM_H = 224
 IM_W = 224
 IM_C = 3
 
-#### Method 1: Using tensorflow methods
 
 def _parse_function(filename, label):
     image_string = tf.read_file(filename)
@@ -71,80 +71,21 @@ dataset = tf.data.Dataset.from_tensor_slices((filenames_placeholder, labels_plac
 dataset = dataset.map(_parse_function)
 
 
-# Plot the tensorboard graph
-# writer = tf.summary.FileWriter('.')
-# writer.add_graph(tf.get_default_graph())
-
-#### End method 1
-
-
-#### Method 2: using standard techniques
-
-# Load images into Numpy arrays
-
-# def load_imgs(path, filenames, new_shape=(224,224), mode='constant'):
-#     """Load an image defined by filenames from a certain path and reshape it 
-#     with the precised mode"""
-
-#     image = KI.img_to_array(KI.load_img(path + filenames[0]))/255
-#     image = resize(image, new_shape, mode=mode)
-#     images = np.array([image])
-#     for i in tqdm(range(1, len(filenames))):
-#         image = KI.img_to_array(KI.load_img(path + filenames[i]))/255
-#         image = resize(image, new_shape, mode=mode)
-#         images = np.append(images, [image], axis=0)
-#     return images
-
-# # Load background images
-# images_bg = load_imgs(PATH_BG, filenames_bg, (IM_H, IM_W))
-
-# # Load dog 1 images
-# images_dog1 = load_imgs(PATH_DOG1, filenames_dog1, (IM_H, IM_W))
-
-# # Define the identies of the dogs: dog1->1, bg->[2,len(bg)]
-# labels_bg = np.arange(2, len(images_bg)+2)
-# labels_dog1 = np.ones(len(images_dog1))
-
-# # Queue images and labels
-# images = np.append(images_dog1, images_bg)
-# labels = np.append(labels_dog1, labels_bg)
-
-# # Method 1: Load data into Tensorflow tensor
-# # dataset = tf.data.Dataset.from_tensor_slices((images, labels))
-
-# # Method 2: Use placeholder
-# assert images.shape[0] == labels.shape[0]
-
-# # Graph elements:
-# images_placeholder = tf.placeholder(images.dtype, images.shape)
-# labels_placeholder = tf.placeholder(labels.dtype, labels.shape)
-
-# # Graph elements:
-# dataset = tf.data.Dataset.from_tensor_slices((images_placeholder, labels_placeholder))
-
-# # Graph elements:
-# iterator = dataset.make_initializable_iterator()
-
-# sess.run(iterator.initializer, feed_dict={images_placeholder: images, labels_placeholder: labels})
-
-#### End Method 2
-
-
 ############################################################
 #  NASNet Graph
 ############################################################
 
-# Start by defining a simple model:
 
-
-
-# Build the model
+# Build the model using Keras pretrained model NASNetMobile,
+# a light and efficient network
 def NASNet_embedding(input_tensor,
   input_shape=(224,224,3),
   include_top=False,
   training=True
 ):
-    base_model = KA.NASNetMobile(input_tensor=input_tensor, input_shape=input_shape, include_top=False)
+    base_model = KA.NASNetMobile(input_tensor=input_tensor,
+                                input_shape=input_shape,
+                                include_top=False)
     x = KL.GlobalAveragePooling2D()(base_model.output)
     x = KL.Dense(1056, activation='relu')(x)
     if training:
@@ -166,28 +107,10 @@ sess.run(init)
 print(sess.run(y_pred))
 
 
-# model = NASNet_embedding(input_shape=(IM_H,IM_W, IM_C))
-# print(model.summary())
-
-
-# With earger execution:
-# tf.enable_eager_execution()
-
-# base_model = KA.NASNetMobile(input_shape=(224,224,3), include_top=False)
-# model = keras.Sequential(
-#   base_model.layers[1:] + [
-#   KL.GlobalAveragePooling2D(),
-#   KL.Dense(1056, activation='relu'),
-#   KL.Dense(128)
-# ])
-
-
-
-
-
 ############################################################
 #  Loss Functions
 ############################################################
+
 
 def triplet_loss(anchor, positive, negative, alpha):
     """Calculate the triplet loss according to the FaceNet paper
